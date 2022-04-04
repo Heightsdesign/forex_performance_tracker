1d4c8b7bdf153a85c81a20f6e672118afc65e67f
Heights Design <plheric@gmail.com>
2022-04-04 21:28:12 +0200

    added database conn

--- fpt/settings.py
+++ fpt/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -92,11 +93,13 @@ DATABASES = {
         'NAME': 'fpt',
         'USER': 'postgres',
         'PASSWORD': 'Eug&nia06240',
-        'HOST': '',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -156,7 +159,7 @@ CHANNEL_LAYERS = {
             'NAME': 'postgres',
             'USER': 'postgres',
             'PASSWORD': 'Eug&nia06240',
-            'HOST': '',
+            'HOST': 'localhost',
             'PORT': '5432',
         },
     },
